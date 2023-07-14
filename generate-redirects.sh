1a951408e8903f8632ac27598d7a4673efcd16eb
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-07-14 23:07:59 +0300

    chore: improve script

--- generate-redirects.sh
+++ generate-redirects.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+rm -rvf html
 yarn && yarn build
 
 for INDEX in `find build -name index.html`; do
-    DIR=${INDEX%index.html}
-    URL="https://wiki.gear.foundation${DIR#build}"
+    OUT=${INDEX#build}
+    DIR=${OUT%index.html}
+    URL="https://wiki.gear.foundation$DIR"
     echo "-> $URL"
-    echo "<html><head><meta http-equiv=\"refresh\" content=\"0; url=$URL\" /></head><body></body></html>" > $INDEX
+    mkdir -p html$DIR
+    echo "<html><head><meta http-equiv=\"refresh\" content=\"0; url=$URL\" /></head><body></body></html>" > html$OUT
 done
 
-echo "---\npermalink: /404.html\n---\n<html><head><meta http-equiv=\"refresh\" content=\"0; url=https://wiki.gear.foundation/\" /></head><body></body></html>" > build/404.html
+echo "---\npermalink: /404.html\n---\n<html><head><meta http-equiv=\"refresh\" content=\"0; url=https://wiki.gear.foundation/\" /></head><body></body></html>" > html/404.html
